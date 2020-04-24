54e159900e23185dca7eb77ccfbe6198347ccb7d
Metin Yıldıran <metin.1464@gmail.com>
2020-04-24 14:36:34 +0300

    item_country done.

--- app/src/main/java/com/example/kotlincountries2/view/CountryFragment.kt
+++ app/src/main/java/com/example/kotlincountries2/view/CountryFragment.kt
@@ -1,11 +1,10 @@
 package com.example.kotlincountries2.view
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.kotlincountries2.CountryFragmentArgs
+import androidx.fragment.app.Fragment
 import com.example.kotlincountries2.R
 
 class CountryFragment : Fragment() {
