8fce95f0b1ab5fe1180f7b733b1117663cdbc377
Metin Yıldıran <metin.1464@gmail.com>
2020-06-02 12:25:43 +0300

    26. ders bitti.

--- app/src/main/java/com/example/kotlincountries2/view/FeedFragment.kt
+++ app/src/main/java/com/example/kotlincountries2/view/FeedFragment.kt
@@ -5,12 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.kotlincountries2.R
 import com.example.kotlincountries2.adapter.CountryAdapter
 import com.example.kotlincountries2.model.Country
+import com.example.kotlincountries2.viewmodel.FeedViewModel
+import kotlinx.android.synthetic.main.fragment_feed.*
 import java.util.ArrayList
 
 class FeedFragment : Fragment() {
+
+    private lateinit var viewModel : FeedViewModel
+    private val countryAdapter = CountryAdapter(arrayListOf())
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,6 +35,24 @@ class FeedFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        viewModel = ViewModelProviders.of(this).get(FeedViewModel::class.java)
+        viewModel.refreshData()
+
+        countryList.layoutManager = LinearLayoutManager(context)    //linear layout manager item_country leri alta alta göstermeye olanak sağlar
+        countryList.adapter = countryAdapter
+
+    }
+
+    fun observeLiveData(){
+        viewModel.countries.observe(this, Observer {
+            //FeedViewModel da countries List<Country> türünde bir değişken, o yüzden bize it, bir List<Country> döndürüyor
+            it?.let {
+                countryList.visibility = View.VISIBLE
+                countryAdapter.updateCountryList(it)
+
+            }
+        })
     }
 
 }
