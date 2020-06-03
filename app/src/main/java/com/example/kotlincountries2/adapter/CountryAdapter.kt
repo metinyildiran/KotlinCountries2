bb33b2ff7491409c28c163b5b4db94cf9c1ddcc1
Metin Yıldıran <metin.1464@gmail.com>
2020-06-03 23:28:21 +0300

    52. ders bitti.

--- app/src/main/java/com/example/kotlincountries2/adapter/CountryAdapter.kt
+++ app/src/main/java/com/example/kotlincountries2/adapter/CountryAdapter.kt
@@ -3,9 +3,11 @@ package com.example.kotlincountries2.adapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.navigation.Navigation
 import androidx.recyclerview.widget.RecyclerView
 import com.example.kotlincountries2.R
+import com.example.kotlincountries2.databinding.ItemCountryBinding
 import com.example.kotlincountries2.model.Country
 import com.example.kotlincountries2.util.downloadFromUrl
 import com.example.kotlincountries2.util.placeholderProgressBar
@@ -14,14 +16,14 @@ import kotlinx.android.synthetic.main.item_country.view.*
 
 class CountryAdapter(val countryList: ArrayList<Country>) : RecyclerView.Adapter<CountryAdapter.CountryViewHolder>() {
 
-    class CountryViewHolder(var view : View) : RecyclerView.ViewHolder(view){
+    class CountryViewHolder(var view : ItemCountryBinding) : RecyclerView.ViewHolder(view.root){    //root binding ile eşleşen view u döndürür
 
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CountryViewHolder {  //item_country ile adapter i birbirine bağlar
         val inflater = LayoutInflater.from(parent.context)
-        val view = inflater.inflate(R.layout.item_country, parent, false)
-
+        //val view = inflater.inflate(R.layout.item_country, parent, false)
+        val view = DataBindingUtil.inflate<ItemCountryBinding>(inflater, R.layout.item_country, parent, false)  //item_country i bağladık, yukarıdakinin DataBinding hali
         return CountryViewHolder(view)
     }
 
@@ -30,6 +32,10 @@ class CountryAdapter(val countryList: ArrayList<Country>) : RecyclerView.Adapter
     }
 
     override fun onBindViewHolder(holder: CountryViewHolder, position: Int) {  //item_country nin componentlarına ulaşmak için
+
+        holder.view.country = countryList[position]
+
+        /*
         holder.view.name.text = countryList[position].countryName  //countryList ten countryName i item_country deki name e atadık
         holder.view.region.text = countryList[position].countryRegion
 
@@ -39,6 +45,7 @@ class CountryAdapter(val countryList: ArrayList<Country>) : RecyclerView.Adapter
         }
 
         holder.view.imageView.downloadFromUrl(countryList[position].imageUrl, placeholderProgressBar(holder.view.context))
+        */
     }
 
     fun updateCountryList(newCountryList : List<Country>){  //yeni gelen verileri eskileri ile yer değiştirir
